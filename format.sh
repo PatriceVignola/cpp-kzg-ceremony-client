ec9255468f7a3fed34c6b4329ee49388654b42c7
Patrice Vignola <vignola.patrice@gmail.com>
2023-01-14 00:29:43 -0800

    Make format.sh lint all C++ files recursively

--- format.sh
+++ format.sh
@@ -7,7 +7,7 @@ NO_COLOR='\033[0m'
 
 # Run cmake-format
 echo -e "${GREEN}Formatting cmake files...${NO_COLOR}"
-find . -iname CMakeLists.txt -o -iname "*.cmake" \
+find . -iname CMakeLists.txt -not \( -path "./build/*" -prune \) -o -iname "*.cmake" \
     -not \( -path "./build/*" -prune \) \
     | xargs cmake-format --in-place
 
@@ -19,10 +19,14 @@ find . -iname CMakeLists.txt -not \( -path "./build/*" -prune \) -o -iname "*.cm
 
 # Run clang-format
 echo -e "${GREEN}Formatting C++ files...${NO_COLOR}"
-find ./src/ -iname *.h -o -iname *.cc | xargs clang-format -i
+find . -iname *.h -not \( -path "./build/*" -prune \) -o -iname *.cc \
+    -not \( -path "./build/*" -prune \) \
+    | xargs clang-format -i
 
 # Run clang-tidy
 echo -e "${GREEN}Linting C++ files...${NO_COLOR}"
-find ./src/ -iname *.h -o -iname *.cc | xargs clang-tidy -p build
+find . -iname *.h -not \( -path "./build/*" -prune \) -o -iname *.cc \
+    -not \( -path "./build/*" -prune \)
+    | xargs clang-tidy -p build
 
 echo -e "${GREEN}Done!${NO_COLOR}"
